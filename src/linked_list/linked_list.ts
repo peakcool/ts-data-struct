3f6b05f1dd920f3879b8dba1085d0ed17d5a9d1a
peakcool <tlc852@qq.com>
2021-04-10 11:49:14 +0800

    feat: add linked list actions

--- src/linked_list/linked_list.ts
+++ src/linked_list/linked_list.ts
@@ -14,41 +14,119 @@ export class LinkedList<T> {
     /** 
      * @Desc:  inserts an element at the beginning of the list.
      */
-    public insertFirst(item: T): void {}
+    public insertFirst(item: T): void {
+        const newNode = new DS.Node<T>(item);
+
+        newNode.next = this.head.next;
+        this.head.next = newNode;
+    }
 
     /** 
      * @Desc:  inserts an element at the end of the list.
      */
-    public insertLast(item: T): void {}
+    public insertLast(item: T): void {
+        const newNode = new DS.Node<T>(item);
+        
+        let current: DS.Node<T> | null = this.head;
+
+        // 找到尾指针前一个结点
+        while(current && current.next !== this.tail) {
+            current = current.next;
+        }
+
+        if (current) {
+            newNode.next = this.tail;
+            current.next = newNode;
+        }
+    }
 
     /** 
      * @Desc:  removes an element at the end of the list.
      */
     public removeFirst(): T | null {
-        return null;
+        if (this.isEmpty()) {
+            throw new Error('linked list is empty');
+        }
+
+        let rv: DS.Node<T> | null = this.head.next;
+
+        if (rv) {
+            this.head.next = rv.next;
+            rv.next = null;
+        }
+
+        return rv ? rv.item : null;
     }
 
     /** 
      * @Desc:  inserts an element with a given key.
      */
-    public remove(item: T): void {}
+    public remove(item: T): T | null {
+        if (this.isEmpty()) {
+            throw new Error('linked list is empty');
+        }
+
+        let current: DS.Node<T> | null = this.head;
+        let rv: DS.Node<T> | null = null;
+
+        while (current.next && current.next.item !== item) {
+            current = current?.next;
+        }
+
+        if (current.next) {
+            rv = current.next;
+            current.next = current.next.next;
+            rv.next = null;
+        }
+
+        return rv && rv.item ? rv.item : null;
+
+        // while(current && current.next !== this.tail) {
+        //     if (current.next && current.next.item === item) {
+        //         current.next = current.next.next;
+        //         break;
+        //     } else {
+        //         current = current.next;
+        //     }
+        // }
+    }
 
     /** 
      * @Desc:  checks to see if an element with a given key is in the list.
      */
     public contains(item: T): boolean {
-        return false;
+
+        if (this.isEmpty()) {
+            return false;
+        }
+
+        let isContain: boolean = false;
+        let current: DS.Node<T> | null = this.head;
+
+        while(current && current.next !== this.tail) {
+            if (current.next && current.next.item === item) {
+                isContain = true;
+                break;
+            }
+            current = current.next;
+        }
+        return isContain;
     }
 
     /** 
      * @Desc:  checks to see if the list contains any node other than head or tail.
      */
-    public isEmpty(item: T): void {}
+    public isEmpty(): boolean {
+        return this.head.next === this.tail;
+    }
 
     /** 
      * @Desc:  retrieve the first element in the list without removing it.
      */
-    public getFirst(item: T): T | null {
-        return null;
+    public getFirst(): T | null {
+        if (this.isEmpty()) {
+            throw new Error('linked list is empty');
+        }
+        return this.head.next ? this.head.next.item : null;
     }
 }
